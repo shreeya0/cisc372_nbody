2529c25add32eea1049462055eaa809020ef5ca2
Shreeya Parekh <shreeya@ubuntu-2004.eecis.udel.edu>
2023-05-09 23:49:01 -0400

    cuda compute

--- compute.cu
+++ compute.cu
@@ -24,4 +24,53 @@ __global__ void pAccComp(vector3 *hPos, vector3 *accels, double *mass){
 			FILL_VECTOR(accels[ind], acc * dist[0] / mag, acc * dist[1] / mag, acc * dist[2]/ mag);
 		}
 	}
+} 
+
+__global__ void sum(vector3 *accels, vector3 *accel_sum, vector3 *hPos, vector3 *hVel){
+	int row = blockIdx.x * blockDim.x + threadIdx.x;
+	int i = row;
+	if (i < NUMENTITIES){
+		FILL_VECTOR(accel_sum[i], 0, 0, 0);
+		for (int j = 0; j < NUMENTITIES; j++){
+			for (int k = 0; k < 3; k++){
+				accel_sum[i][k] += accels[(i * NUMENTITIES) + j][k];
+			}
+		}
+		for (int k = 0; k < 3; k++){
+			hVel[i][k] += accel_sum[i][k] * INTERVAL;
+			hPos[i][k] = hVel[i][k] * INTERVAL;
+		}
+
+	}
+}
+
+void compute(){
+	vector3 *dhPos, *dhVel, *dacc, *dsum;
+	double *dmass;
+	int block = ceilf(NUMENTITIES / 16.0f);
+	int thread = ceilf(NUMENTITIES / (float) block);
+	dim3 gridDim(block, block, 1);
+	dim3 gridDim(thread, thread, 1);
+	
+	cudaMalloc((void**) &dhPos, sizeof(vector3) * NUMENTITIES);
+	cudaMalloc((void**) &dhVel, sizeof(vector3) * NUMENTITIES);
+	cudaMalloc((void**) &dacc, sizeof(vector3) * NUMENTITIES);
+	cudaMalloc((void**) &dsum, sizeof(vector3) * NUMENTITIES);
+	cudaMalloc((void**) &dmass, sizeof(double) * NUMENTITIES);
+
+	cudaMemcpy(dhPos, hPos, sizeof(vector3)*NUMENTITIES, cudaMemcpyHostToDevice);
+	cudaMemcpy(dhVel, hVel, sizeof(vector3)*NUMENTITIES, cudaMemcpyHostToDevice);
+	cudaMemcpy(dmass, mass, sizeof(double)*NUMENTITIES, cudaMemcpyHostToDevice);
+	
+	pAccComp<<gridDim.x blockDim.x>>(dacc, dsum, dhPos, dhVel);
+	cudaDeviceSynchronize();
+
+	cudaMemcpy(hPos, dhPos, sizeof(vector3)*NUMENTITIES, cudaMemcpyDeviceToDevice);
+	cudaMemcpy(hVel, dVel, sizeof(vector3)*NUMENTITIES, cudaMemcpyDeviceToDevice);
+
+	cudaFree(dhPos);
+	cudaFree(dVel);
+	cudaFree(dmass);
+	cudaFree(dacc);
+
 }
